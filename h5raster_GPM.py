f448561f7670a7fc9ad6ffd6bd2c4bca06bcb3a8
R. Edward Beighley <r.beighley@neu.edu>
2016-02-25 10:19:50 -0500

    Update h5raster_GPM.py

--- h5raster_GPM.py
+++ h5raster_GPM.py
@@ -1,7 +1,29 @@
+#This code will open GPM H5 files and create ArcGIS Rasters
+
 import h5py
 import numpy
-import rio
+import arcpy
 
+# contants and envitonments
+arcpy.env.overwriteOutput = True
+
+def DefineSpatialReferenceAndSave(in_array, path, x_min, y_min, cell_x, cell_y, no_data, spatial_ref, silent = False):
+	"""DefineSpatialReferenceAndSave(in_array, path, x_min, y_min, cell_x, cell_y{, no_data = None}{, spatial_ref = None}{, silent = False})
+convert input array to raster, define coordinate system and projection, then save to a file.
+arguments
+in_array: array to convert. (NumPy ndarray)
+path: save path. (string)
+x_min: left edge coordinate. (number)
+y_min: bottom edge coordinate. (number)
+cell_x: horizontal length of a pixel. (number)
+cell_y: vertical length of a pixel. (number)
+no_data: 'no data' value of created raster. (value)
+spatial_ref: path of a raster for spatial references. (arcpy spatialReference)
+silent: quiet mode. (boolean)"""
+	if not silent: print "DEFINING COORD SYSTEM & SAVING:", path
+	raster = arcpy.NumPyArrayToRaster(in_array, arcpy.Point(x_min, y_min), cell_x, cell_y, no_data)
+	arcpy.DefineProjection_management(raster, arcpy.SpatialReference(spatial_ref))
+	raster.save(path)
 
 X_MIN = -180.0
 Y_MIN = -90.0
@@ -11,7 +33,7 @@ NO_DATA = -9999.9
 SR = 4326 #wgs84
 
 
-f = open("list.txt", "r")  ###
+f = open("list.txt", "r")  #list containing all .H5 file names
 LIST = []
 for l in f:
 	LIST.append(l[:-1])
@@ -20,10 +42,11 @@ f.close()
 for fi in LIST:
 	s = fi.split(".")[3]
 	f = h5py.File(fi, "r")
-	sm = f["Grid"]["precipitationCal"][:]
+	sm = f["Grid"]["precipitationCal"][:] #calibrated precip values from GPM-IMERG H5 files
 	
 	sm = sm.transpose()
 	sm = numpy.flipud(sm)
 	
 	f.close()
-	rio.DefineSpatialReferenceAndSave(sm, "C:\\Research\\SMAP\\" + s, X_MIN, Y_MIN, L_X, L_Y, NO_DATA, SR)
+	DefineSpatialReferenceAndSave(sm, "C:\\Research\\SMAP\\" + s, X_MIN, Y_MIN, L_X, L_Y, NO_DATA, SR)
+
